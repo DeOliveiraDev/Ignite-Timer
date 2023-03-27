c5b90d91ea2d194dc7224fa63b748c033b5ac789
Vinícius Oliveira <deoliveiradev@gmail.com>
2023-03-27 00:03:09 -0300

    feat: creating ContextApi for Cycles data

---
+++ src/pages/Home/NewCycleForm/index.tsx
@@ -0,0 +1,46 @@
+import { MinutesAmoutInput, FormContainer, TaskInput } from './styles'
+import { useContext } from 'react'
+import { CyclesContext } from '..'
+import { useFormContext } from 'react-hook-form'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="De um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      {/* datalist serves to create a list of suggestions for the user 
+    when clicking on the input that references the datalist by its id. 
+    */}
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+        <option value="Banana" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmoutInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
